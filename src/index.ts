a239dfe818f5f94d07e7106381986f48600128f3
Aditya <111768380+adibackend@users.noreply.github.com>
2025-01-04 18:31:35 +0000

    Db is working but post route doesn't

--- src/index.ts
+++ src/index.ts
@@ -10,23 +10,23 @@ app.get('/',(req,res)=>{
     res.json({messeage: "everything is correct"})
 })
 
-app.post('api/v1/signup',(req,res)=>{
+app.post('/api/v1/signup',(req,res)=>{
     //zod validation +hash the password 
+    console.log('inside post method')
     const {username,password}=req.body
-
     UserModel.create({username,password})
     res.json({messeage:'user has signed up'})
 })
-app.post('api/v1/sigin',(req,res)=>{
+app.post('/api/v1/sigin',(req,res)=>{
 
 })
-app.post('api/v1/content',(req,res)=>{
+app.post('/api/v1/content',(req,res)=>{
 
 })
-app.post('api/v1/brain/share',(req,res)=>{
+app.post('/api/v1/brain/share',(req,res)=>{
 
 })
-app.post('api/v1/brain/:sharelink',(req,res)=>{
+app.post('/api/v1/brain/:sharelink',(req,res)=>{
 
 })
 
